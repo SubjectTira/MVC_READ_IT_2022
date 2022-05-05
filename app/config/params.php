478874edb66aa291b964ea29384c6fb77ac5758c
SubjectTira <fassmannjeremie@gmail.com>
2022-05-05 15:17:34 +0200

    Structure MVC de base

---
+++ app/config/params.php
@@ -0,0 +1,12 @@
+<?php 
+
+// initialisation des zones dynamiques
+
+$content = "";
+
+// Paremetres de connexion
+
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'readit_2020');
+define('DB_USER', 'root');
+define('DB_PWD', 'root');
