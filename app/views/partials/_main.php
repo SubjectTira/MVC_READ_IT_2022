cb59a7d5c93558ac6f395be79d336a2b85516d73
SubjectTira <fassmannjeremie@gmail.com>
2022-05-12 13:35:23 +0200

    Découpe du template en partials

---
+++ app/views/partials/_main.php
@@ -0,0 +1,10 @@
+<section class="ftco-section ftco-degree-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 ftco-animate">
+            <?php echo $content ?>
+          </div>
+
+          <!-- .col-md-8 -->
+          <?php include '../app/views/partials/_aside.php'; ?>
+    </section> <!-- .section -->
